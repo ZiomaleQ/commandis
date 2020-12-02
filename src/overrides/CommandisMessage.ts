d982c08b8df2dde13ee08fb8b2b9504873f595f4
ZiomaleQ <r.partyka30@gmail.com>
2020-12-02 14:48:54 +0100

    Message property is not necessary

--- src/overrides/CommandisMessage.ts
+++ src/overrides/CommandisMessage.ts
@@ -4,12 +4,10 @@ import { StringReader } from "../stringReader.ts";
 
 
 export class CommandisMessage extends Message {
-    message: Message;
     stringReader: StringReader;
 
     constructor(message: Message, client: Client, stringReader?: StringReader) {
         super(message.data, client, message.channel, message.guild)
-        this.message = message;
         this.stringReader = stringReader ?? new StringReader("", 0);
     }
 }
