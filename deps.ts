1a7ede676d43772a7dae6cf346806db18b4539e9
ZiomaleQ <r.partyka30@gmail.com>
2021-08-07 10:50:05 +0200

    Bumping corddis version

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export * from "https://deno.land/x/corddis@v0.11.2/mod.ts";
+export * from "https://deno.land/x/corddis@v0.11.3/mod.ts";
 export { default as EventEmitter } from "https://deno.land/x/events/mod.ts";
